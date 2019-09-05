385de1941319a52752dfe195b87c245cbc109aec
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-09-05 10:20:24 +0100

    Refactor starship and details page

---
+++ src/views/Starship.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="home">
+    <Header></Header>
+
+    <main class="main">
+      <div class="main__primary-title">
+        Popular Starships
+      </div>
+
+      <div class="starships-container">
+        <div class="starship">
+          <img src="../assets/starship-1.jpg" class="starship__image">
+
+          <div class="starship__summary">
+            <p class="starship__name">Ghost</p>
+            <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+
+            <button class="starship__button" @click="details">Read More &nbsp; &rarr; </button>
+          </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+          <img src="../assets/starship-1.jpg" class="starship__image">
+
+          <div class="starship__summary">
+            <p class="starship__name">Ghost</p>
+            <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+
+            <button class="starship__button">Read More &nbsp; &rarr; </button>
+          </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-1.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+      </div>
+
+      <div class="button__view-more">
+        view more
+      </div>    
+
+    </main>
+
+    <footer class="footer">
+      &nbsp;
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+  name: 'Starship',
+  data: function () {
+    return {
+    }
+  },
+  components: {
+    Header
+  },
+  methods: {
+    details: function () {
+      this.$router.push({path: 'starship-details'})
+    }
+  }
+  
+}
+</script>
---
+++ src/views/starshipDetails.vue
@@ -0,0 +1,107 @@
+
+<template>
+  <div class="starship-1">
+    <header class="header">
+      <img class="header__logo small-logo" src="../assets/logo.png" alt="">
+      <img src="../assets/starship-2.jpg" alt="" class="header__bg">
+
+      <div class="header__content">
+        <div class="header__content-starship">
+          <div class="header__starship-name">
+            {{details.name}}
+          </div>
+
+          <div class="header__pagination">
+            <ul class="header__pagination-controls">
+              <li class="header__pagination-arrow"></li>
+              <li class="header__pagination-arrow"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <main class="main">
+      <div class="starship__main-detail">
+        <h2 class="starship__main-name">{{details.name}}</h2>
+
+        <p class="starship__main-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, sapiente culpa possimus consectetur maxime veniam dolorem et itaque laboriosam rem fuga voluptatem temporibus sed, mollitia aliquid. Officia ipsa minima velit.
+
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate quae, repellendus neque nam porro aliquid tempore, quis, debitis a ullam sapiente cumque facilis eveniet. Quos nihil minima quae iusto autem.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio magni nostrum quo cupiditate ipsum doloribus doloremque nesciunt ratione impedit omnis non minima excepturi ducimus sint, consequatur atque itaque est?
+
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, saepe necessitatibus nemo quas rerum libero recusandae inventore, eos repellat quos maxime repudiandae. Fuga repellat labore deleniti nesciunt nihil incidunt sequi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, repellat possimus? Voluptate nostrum sunt ullam atque assumenda quas velit quo in ab quae explicabo nulla, vel reprehenderit cumque modi dolorem!
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis natus, dolore quod necessitatibus voluptas at eum sit reiciendis quam fuga obcaecati ab! Exercitationem delectus eveniet inventore dolorum, esse harum.
+        </p>
+      </div>
+
+
+      <div class="main__secondary-title">
+        Recently Viewed Starships
+      </div>
+
+      <hr class="divider" />
+
+      <div class="starships-container">
+        <div class="starship">
+          <img src="../assets/starship-1.jpg" class="starship__image">
+
+          <div class="starship__summary">
+            <p class="starship__name">Ghost</p>
+            <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+
+            <button class="starship__button">Read More &nbsp; &rarr; </button>
+          </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-2.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/starship-3.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+      </div>
+    </main>
+
+    <footer class="footer">
+      &nbsp;
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'starshipDetails',
+  data: function () {
+    return {
+      details: ''
+    }
+  },
+  created () {
+    this.details = this.$route.params
+    if (!this.$route.params) {
+      this.$router.push('/starship')
+    }
+  }
+  
+}
+</script>
