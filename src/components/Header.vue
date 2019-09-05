c008a49ec6291a25ceb42a7b97f1d051dfd20ee2
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-09-05 10:19:51 +0100

    Include header components

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <img class="header__logo small-logo" src="../assets/logo.png" alt="">
+    <img src="../assets/hero-banner.jpg" alt="" class="header__bg">
+
+    <div class="header__content">
+      <div class="header__content-main">
+        <div class="header__title">
+          <img src="../assets/logo.png" alt="" class="header__title-img small-logo">
+          <p class="header__title-text">Directory</p>
+        </div>
+
+        <div class="header__title-secondary">
+          Find your favorite Characters, Films, Species, Starships and Planets
+        </div>
+
+        <div class="header__search-container">
+          <i class="fa fa-search header__search-icon"></i>
+          <input type="text" class="header__search-box" placeholder="Enter a search term">
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
