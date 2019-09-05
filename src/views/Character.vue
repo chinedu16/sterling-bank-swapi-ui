2a44b3b2e056488d0942665b7b445bf18e6abbd1
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-09-05 10:20:15 +0100

    Refactor characters and details page

---
+++ src/views/Character.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="about">
+    <Header></Header>
+
+    <main class="main">
+      <div class="main__primary-title">
+        Starwars Characters
+      </div>
+
+      <div class="filters">
+        <div class="form-group">
+          <label for="gender" class="form-label">filter</label>
+          <select name="gender" id="">
+            <option value="male" selected>male</option>
+            <option value="female">female</option>
+          </select>
+        </div>
+        <div class="form-group">
+            <label for="gender" class="form-label">view</label>
+            <select name="gender" id="">
+              <option value="grid" selected>grid</option>
+              <option value="list">list</option>
+            </select>
+          </div>
+      </div>
+
+      <div class="characters-container">
+        <div class="character">
+          <img src="../assets/character-1.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore" @click="details">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-1.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-3.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-3.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-1.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-1.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-2.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+
+        <div class="character">
+          <img src="../assets/character-2.jpg" alt="" class="character__image">
+
+          <div class="character__summary">
+            <p class="character__name">Luke Skywalker</p>
+            <p class="character__subtitle">Son of Anakin</p>
+            <p class="character__detail">Anakin Skywalker, a Force-sensitive human male, was a legendary Jedi master who fought in the Galactic Civil War during the reign of the Galactic empire...<span class="button__inline-readmore">Read More</span></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="pagination">
+        <div class="pagination__text">
+          1-10 of 50
+        </div>
+
+        <ul class="pagination__controls">
+          <li class="pagination__arrow"></li>
+          <li class="pagination__arrow"></li>
+        </ul>
+      </div>
+    </main>
+
+    <footer class="footer">
+      &nbsp;
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+  name: 'About',
+  data: function () {
+    return {
+    }
+  },
+  components: {
+    Header
+  },
+  methods: {
+    details:  function () {
+    this.$router.push({path: '/character-details'})
+    }
+  }
+}
+</script>
---
+++ src/views/characterDetails.vue
@@ -0,0 +1,86 @@
+
+<template>
+  <div class="starship-1">
+    <Header></Header>
+
+    <main class="main">
+      <div class="starship__main-detail">
+        <h2 class="starship__main-name">Correllian Scout</h2>
+
+        <p class="starship__main-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, sapiente culpa possimus consectetur maxime veniam dolorem et itaque laboriosam rem fuga voluptatem temporibus sed, mollitia aliquid. Officia ipsa minima velit.
+
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate quae, repellendus neque nam porro aliquid tempore, quis, debitis a ullam sapiente cumque facilis eveniet. Quos nihil minima quae iusto autem.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio magni nostrum quo cupiditate ipsum doloribus doloremque nesciunt ratione impedit omnis non minima excepturi ducimus sint, consequatur atque itaque est?
+
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, saepe necessitatibus nemo quas rerum libero recusandae inventore, eos repellat quos maxime repudiandae. Fuga repellat labore deleniti nesciunt nihil incidunt sequi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, repellat possimus? Voluptate nostrum sunt ullam atque assumenda quas velit quo in ab quae explicabo nulla, vel reprehenderit cumque modi dolorem!
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis natus, dolore quod necessitatibus voluptas at eum sit reiciendis quam fuga obcaecati ab! Exercitationem delectus eveniet inventore dolorum, esse harum.
+        </p>
+      </div>
+
+
+      <div class="main__secondary-title">
+        Recently Viewed Starships
+      </div>
+
+      <hr class="divider" />
+
+      <div class="starships-container">
+        <div class="starship">
+          <img src="../assets/character-1.jpg" class="starship__image">
+
+          <div class="starship__summary">
+            <p class="starship__name">Ghost</p>
+            <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+
+            <button class="starship__button">Read More &nbsp; &rarr; </button>
+          </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/character-2.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+
+        <div class="starship">
+            <img src="../assets/character-3.jpg" class="starship__image">
+  
+            <div class="starship__summary">
+              <p class="starship__name">Ghost</p>
+              <p class="starship__description">The Ghost is a modified VCX-100 light freighter, manufactured by the Corelian Engineering Corporation.</p>
+  
+              <button class="starship__button">Read More &nbsp; &rarr; </button>
+            </div>
+        </div>
+      </div>
+    </main>
+
+    <footer class="footer">
+      &nbsp;
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+  name: 'starshipDetails',
+  data: function () {
+    return {
+    }
+  },
+  components: {
+    Header
+  }
+  
+}
+</script>
