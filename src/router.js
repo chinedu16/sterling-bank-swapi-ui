b26335c9a687b9a3a8ab484c3141743b09801f22
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-09-05 10:19:03 +0100

    Refactor config files

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Starship from './views/Starship.vue'
+import starshipDetails from './views/starshipDetails.vue'
 
 Vue.use(Router)
 
@@ -14,12 +16,24 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/starship',
+      name: 'starship',
+      component: Starship
+    },
+    {
+      path: '/starship-details',
+      name: 'starship-details',
+      component: starshipDetails
+    },
+    {
+      path: '/character',
+      name: 'character',
+      component: () => import('./views/Character.vue')
+    },
+    {
+      path: '/character-details',
+      name: 'character-details',
+      component: () => import('./views/characterDetails.vue')
     }
   ]
 })
