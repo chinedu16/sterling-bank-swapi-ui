8aecdfffbe37ad722a12f34beccee158e2d0c016
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-09-05 13:59:05 +0100

    Refator Config

--- src/api/index.js
+++ src/api/index.js
@@ -35,13 +35,51 @@ async function characterPrev (url) {
 }
 
 
+async function listPlanet() {
+  const response = await axios.get(`${baseUrl}/planets/`)
+  return response
+}
+
+async function planetNext (url) {
+  const response = await axios.get(url)
+  return response
+}
+
+async function planetPrev (url) {
+  const response = await axios.get(url)
+  return response
+}
+
+
+async function planetSearch(search) {
+  const response = await axios.get(`${baseUrl}/planets/?search=${search}`)
+  return response
+}
+
+async function characterSearch(search) {
+  const response = await axios.get(`${baseUrl}/people/?search=${search}`)
+  return response
+}
+
+async function starshipSearch(search) {
+  const response = await axios.get(`${baseUrl}/starships/?search=${search}`)
+  return response
+}
+
 export default {
   listStarship,
   starshipNext,
   starshipPrev,
+  starshipSearch,
 
   listCharacter,
   characterNext,
   characterPrev,
+  characterSearch,
+
+  listPlanet,
+  planetNext,
+  planetPrev,
+  planetSearch
 
 }
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Starship from './views/Starship.vue'
 import starshipDetails from './views/starshipDetails.vue'
+import Planet from './views/Planet.vue'
 
 Vue.use(Router)
 
@@ -34,6 +35,16 @@ export default new Router({
       path: '/character-details',
       name: 'character-details',
       component: () => import('./views/characterDetails.vue')
-    }
+    },
+    {
+      path: '/planet',
+      name: 'planet',
+      component: Planet
+    },
+    {
+      path: '/planet-details',
+      name: 'planet-details',
+      component: () => import('./views/planetDetails.vue')
+    },
   ]
 })
